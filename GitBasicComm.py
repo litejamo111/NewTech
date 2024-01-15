815418b621148fefccbffc365ec5380c7c6a6681
desarrollo <litejamo2@hotmail.com>
2024-01-15 07:17:05 -0500

    se agrego otro lorem

--- GitBasicComm.py
+++ GitBasicComm.py
@@ -3,4 +3,5 @@ str text="commandos basicos de git"
 str lorem="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
 
 print(lorem)
- Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ Lorem ipsum dolor sit.
