8b29d91735dcb41666b5ca318bcb51650206ac65
litejamo111 <litejamo2hot@>
2024-01-18 06:42:57 -0500

    creacion de nuevos archivos

---
+++ age.py
@@ -0,0 +1,4 @@
+name="kratos"
+age=1000
+if(age>=18):print("es mayor de edad")
+else:print("no es mayor de edad")
---
+++ pair.py
@@ -0,0 +1,5 @@
+n1=2
+n2=3
+if(n1%2==0):
+    print("es par")
+else: print("no es par")
