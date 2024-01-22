6e28d5f763255772cb4e531a6818bbd9cb8b335e
litejamo111 <litejamo2@hotmail.com>
2024-01-22 07:15:23 -0500

    nueva_funcion_agregada

--- age.py
+++ age.py
@@ -1,4 +1,13 @@
-name=input("ingrese su nombre")
-age=input("ingrese su edad")
-if(age>=18):print("es mayor de edad")
-else:print("no es mayor de edad")
+name=input("ingrese su nombre: ")
+age=int(input("ingrese su edad: "))
+if(age>=18):
+    id=input("ingrese su cedula: ")
+    civilstatus=input("ingrese su estado civil: ")
+    exdata=input("Tiene trabajo? SI/NO: ")
+    if(exdata=="SI"):
+        print("\nNombre:  "+name+"\n"+"edad: "+str(age)+"\n"+"id: "+id+"\n"+"estado civil"+civilstatus+"\n"+"estado laboral: empleado")
+    elif(exdata=="NO"):
+        print("\nNombre:  "+name+"\n"+"edad: "+str(age)+"\n"+"id: "+id+"\n"+"estado civil"+civilstatus+"\n"+"estado laboral: desempleado")
+else:
+    Family=input("con quien vive?: ")
+    print("\nno es mayor de edad"+"\nvive con su: "+Family)
