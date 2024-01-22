fea11c60f175a1c38b124bc24af18b804046a9f9
desarrollo <yeisonmorenoberrio@gmail.com>
2024-01-22 07:20:28 -0500

    correccrion

--- pair.py
+++ pair.py
@@ -3,11 +3,11 @@ n2=input("ingrese un numero par")
 n3 =3
 if(n1%2==0):
     print("es par")
-elif: 
-    print("no es par")
-    elif: (n2=="")
+    elif: (n2==""):
     print("campo vacio ingrese un numero")
     else:
         print("error")
+
+
 
 
