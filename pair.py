60f5d9b375f79591355d7db986730f600e77f9e6
desarrollo <yeisonmorenoberrio@gmail.com>
2024-01-22 06:48:16 -0500

    agregada condicional en pair

--- pair.py
+++ pair.py
@@ -3,4 +3,9 @@ n2=input("ingrese un numero par")
 n3 =3
 if(n1%2==0):
     print("es par")
-else: print("no es par")
+else if: 
+    print("no es par")
+    else if(n1=="")
+    print("objeto vacio ingrese un numero")
+
+
